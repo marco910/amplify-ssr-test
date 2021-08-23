24c928761599a7c7288d8514ac2ee7dcf7c49333
Marco Feicht <57726854+marco910@users.noreply.github.com>
2021-08-23 16:42:10 +0200

    Some changes and a new API Get route

--- pages/index.js
+++ pages/index.js
@@ -1,93 +1,21 @@
-import { Text, Input, Textarea, Button } from "@geist-ui/react";
-import { useState } from "react";
-import axios from "axios";
+import Link from "next/link";
 
-export default function IndexPage() {
-  const input_props = {
-    width: "100%"
-  };
-
-  let [formData, setFormData] = useState({
-    first_name: "",
-    last_name: "",
-    subject: "",
-    message: ""
-  });
-
-  let [message, setMessage] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      axios({
-        method: "POST",
-        url: "/api/send",
-        data: formData
-      })
-        .then(({ data }) => console.log(data))
-        .catch((err) => setMessage("A error occurred"));
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
+export default function () {
   return (
-    <div className="container">
-      <Text h1>Contact Form</Text>
-
-      <form onSubmit={handleSubmit}>
-        <div className="grid">
-          <div className="col-md-6 col-xs-12">
-            <Input
-              placeholder="First name"
-              name="first_name"
-              onChange={(e) =>
-                setFormData({ ...formData, first_name: e.target.value })
-              }
-              value={formData.first_name}
-              {...input_props}
-            />
-          </div>
-          <div className="col-md-6 col-xs-12">
-            <Input
-              placeholder="Last name"
-              name="last_name"
-              {...input_props}
-              onChange={(e) =>
-                setFormData({ ...formData, last_name: e.target.value })
-              }
-              value={formData.last_name}
-            />
-          </div>
-          <div className="col-md-12 col-xs-12">
-            <Input
-              placeholder="Subject"
-              name="subject"
-              {...input_props}
-              onChange={(e) =>
-                setFormData({ ...formData, subject: e.target.value })
-              }
-              value={formData.subject}
-            />
-          </div>
-          <div className="col-md-12 col-xs-12">
-            <Textarea
-              placeholder="Message"
-              name="message"
-              {...input_props}
-              onChange={(e) =>
-                setFormData({ ...formData, message: e.target.value })
-              }
-              value={formData.message}
-            />
-          </div>
-          <div className="col-md-12 col-xs-12">
-            <button type="submit">Send</button>
-          </div>
-        </div>
-      </form>
-
-      {message !== "" && <div>{message}</div>}
+    <div>
+      <h1>Hello World from Next.js</h1>
+      <ul>
+        <li>
+          <Link href="/api/test">
+            <a>API GET Test</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact Form</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
